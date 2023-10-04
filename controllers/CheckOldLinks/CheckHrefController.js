f18bfa481fde3cc212dffd474602aff40503c390
Ishika Ranjan <ishikaranjan8@gmail.com>
2023-10-04 19:48:32 +0530

    puppeteer

--- controllers/CheckOldLinks/CheckHrefController.js
+++ controllers/CheckOldLinks/CheckHrefController.js
@@ -49,7 +49,10 @@ exports.checkHref = async (req, res) => {
       let status = entry.Status;
       let docId = `${collection.name}_${entry._id}`;
 
-      const browser = await puppeteer.launch({ headless: "new" });
+      const browser = await puppeteer.launch({ 
+        headless: "new",
+        args: ["--no-sandbox"]
+      });
       const page = await browser.newPage();
 
       try {
