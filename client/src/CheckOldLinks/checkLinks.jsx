9f8ee3deb0b2a0aaec8eb472726dd2b3a7fe032b
Ishika Ranjan <ishikaranjan8@gmail.com>
2023-10-05 19:15:55 +0530

    change COL table bootstrap

--- client/src/CheckOldLinks/checkLinks.jsx
+++ client/src/CheckOldLinks/checkLinks.jsx
@@ -26,66 +26,66 @@ const Table = () => {
 
   return (
     <div>
-      <h1>Check Href Page</h1>
+      <h1 className="m-4 text-center">Check Old Links</h1>
         <div>
         {Object.entries(updatedData).map(([key, value]) => (
-      <div key={key} className='p-5'>
-        <h2 className="text-center">{key}</h2>
-        <div className="table-responsive"  > {/* Center the table */}
-          <table className="table table-bordered"> {/* Limit the table's maximum width */}
-            <thead>
-              <tr>
-                <th>Mailbox</th>
-                <th>Email</th>
-                <th>DF</th>
-                <th>Topic</th>
-                <th>LTE</th>
-                <th>AnchorText</th>
-                <th>PublishedLink</th>
-                <th>Status</th>
-                <th>New AnchorText</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {value.map((row) => (
-                <tr key={row._id}>
-                  <td>{row.websiteRow.Mailbox}</td>
-                  <td>{row.websiteRow.Email}</td>
-                  <td>{row.websiteRow.DF}</td>
-                  <td>{row.websiteRow.Topic}</td>
-                  <td>{row.websiteRow.LTE}</td>
-                  <td>{row.websiteRow.AnchorText}</td>
-                  <td>{row.websiteRow.PublishedLink}</td>
-                  <td>{row.websiteRow.Status}</td>
-                  <td>{row.newAnchor}</td>
-                  <td>
-                    {row.newAnchor ? (
-                      <button
-                        className="btn btn-primary"
-                        onClick={() => handleUpdate(row.websiteRow._id)}
-                      >
-                        Update
-                      </button>
-                    ) : (
-                      <button className="btn btn-secondary" disabled>
-                        Update
-                      </button>
-                    )}
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => handleDelete(row.websiteRow._id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    ))}
+  <div key={key} className='p-5'>
+    <h2 className="text-center">{key}</h2>
+    <div className="table-responsive" style={{ textAlign: 'center' }}>
+      <table className="table table-bordered" style={{ width: '100%' }}>
+        <thead>
+          <tr>
+            <th style={{ width: '10%' }}>Mailbox</th>
+            <th style={{ width: '10%' }}>Email</th>
+            <th style={{ width: '10%' }}>DF</th>
+            <th style={{ width: '10%' }}>Topic</th>
+            <th style={{ width: '10%' }}>LTE</th>
+            <th style={{ width: '10%' }}>AnchorText</th>
+            <th style={{ width: '10%' }}>PublishedLink</th>
+            <th style={{ width: '10%' }}>Status</th>
+            <th style={{ width: '10%' }}>New AnchorText</th>
+            <th style={{ width: '10%' }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {value.map((row) => (
+            <tr key={row._id}>
+              <td style={{ width: '10%' }}>{row.websiteRow.Mailbox}</td>
+              <td style={{ width: '10%' }}>{row.websiteRow.Email}</td>
+              <td style={{ width: '10%' }}>{row.websiteRow.DF}</td>
+              <td style={{ width: '10%' }}>{row.websiteRow.Topic}</td>
+              <td style={{ width: '10%' }}>{row.websiteRow.LTE}</td>
+              <td style={{ width: '10%' }}>{row.websiteRow.AnchorText}</td>
+              <td style={{ width: '10%' }}>{row.websiteRow.PublishedLink}</td>
+              <td style={{ width: '10%' }}>{row.websiteRow.Status}</td>
+              <td style={{ width: '10%' }}>{row.newAnchor}</td>
+              <td style={{ width: '10%' }}>
+                {row.newAnchor ? (
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleUpdate(row.websiteRow._id)}
+                  >
+                    Update
+                  </button>
+                ) : (
+                  <button className="btn btn-secondary" disabled>
+                    Update
+                  </button>
+                )}
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(row.websiteRow._id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+))}
         </div>
     </div>
   );
