d66c9addc557019b79d9db9226899e15be3f7966
Ishika Ranjan <ishikaranjan8@gmail.com>
2023-10-05 19:25:55 +0530

    .

--- client/src/CheckOldLinks/checkLinks.jsx
+++ client/src/CheckOldLinks/checkLinks.jsx
@@ -29,34 +29,34 @@ const Table = () => {
           <div key={key} className='p-5'>
             <h2 className="text-center">{key}</h2>
             <div className="table-responsive" style={{ textAlign: 'center' }}>
-              <table className="table table-bordered" style={{ maxWidth: '100%', wordWrap: 'break-word' }}>
+              <table className="table table-bordered" style={{ width: '100%' }}>
                 <thead>
                   <tr>
-                    <th style={{ width: '10%' }}>Mailbox</th>
-                    <th style={{ width: '10%' }}>Email</th>
-                    <th style={{ width: '10%' }}>DF</th>
-                    <th style={{ width: '10%' }}>Topic</th>
-                    <th style={{ width: '10%' }}>LTE</th>
-                    <th style={{ width: '10%' }}>AnchorText</th>
-                    <th style={{ width: '10%' }}>PublishedLink</th>
-                    <th style={{ width: '10%' }}>Status</th>
-                    <th style={{ width: '10%' }}>New AnchorText</th>
-                    <th style={{ width: '10%' }}>Action</th>
+                    <th style={{ maxWidth: '10%' }}>Mailbox</th>
+                    <th style={{ maxWidth: '10%' }}>Email</th>
+                    <th style={{ maxWidth: '10%' }}>DF</th>
+                    <th style={{ maxWidth: '10%' }}>Topic</th>
+                    <th style={{ maxWidth: '10%' }}>LTE</th>
+                    <th style={{ maxWidth: '10%' }}>AnchorText</th>
+                    <th style={{ maxWidth: '10%' }}>PublishedLink</th>
+                    <th style={{ maxWidth: '10%' }}>Status</th>
+                    <th style={{ maxWidth: '10%' }}>New AnchorText</th>
+                    <th style={{ maxWidth: '10%' }}>Action</th>
                   </tr>
                 </thead>
                 <tbody>
                   {value.map((row) => (
                     <tr key={row._id}>
-                      <td style={{ width: '10%' }}>{row.websiteRow.Mailbox}</td>
-                      <td style={{ width: '10%' }}>{row.websiteRow.Email}</td>
-                      <td style={{ width: '10%' }}>{row.websiteRow.DF}</td>
-                      <td style={{ width: '10%' }}>{row.websiteRow.Topic}</td>
-                      <td style={{ width: '10%' }}>{row.websiteRow.LTE}</td>
-                      <td style={{ width: '10%' }}>{row.websiteRow.AnchorText}</td>
-                      <td style={{ width: '10%' }}>{row.websiteRow.PublishedLink}</td>
-                      <td style={{ width: '10%' }}>{row.websiteRow.Status}</td>
-                      <td style={{ width: '10%' }}>{row.newAnchor}</td>
-                      <td style={{ width: '10%' }}>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.websiteRow.Mailbox}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.websiteRow.Email}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.websiteRow.DF}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.websiteRow.Topic}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.websiteRow.LTE}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.websiteRow.AnchorText}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.websiteRow.PublishedLink}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.websiteRow.Status}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>{row.newAnchor}</td>
+                      <td style={{ maxWidth: '10%', wordWrap: 'break-word' }}>
                         {row.newAnchor ? (
                           <button
                             className="btn btn-primary"
